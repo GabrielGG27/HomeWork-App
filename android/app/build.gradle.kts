9000e064b15f3d5aef4008de092ec541bf1ccd9d
GabrielGG <garciagodinezgabriel@cecyt12.edu.mx>
2026-05-13 16:13:41 -0600

    version_commit

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -37,8 +37,8 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 24
         targetSdk = 36
-        versionCode = 20
-        versionName = "1.0.20+20"
+        versionCode = 21
+        versionName = "1.0.21+21"
     }
 
     signingConfigs {
